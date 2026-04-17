564dc0703e5cc83f5baff18bc9a8ed7d6cffe559
Asindu De Silva <asindudesilva@gmail.com>
2026-04-18 01:02:37 +0530

    fix: update router prompt

--- backend/src/agents/tools/catalog_search_tool.py
+++ backend/src/agents/tools/catalog_search_tool.py
@@ -2,5 +2,7 @@ from src.db.qdrant_db_client import search_products
 
 class CatalogSearchTool:
      def search(self, vector_query: str, keyword_query: str,):
-        return search_products(vector_query, keyword_query)
-
+        return search_products(
+            vector_query.replace("gifts", "options"),
+            keyword_query
+        )
