0bdbe676ad487778cf3027dd01d220a4f5fbafb1
Asindu De Silva <asindudesilva@gmail.com>
2026-04-22 17:42:47 +0530

    fix: bug fixed

--- backend/src/api/db/core.py
+++ backend/src/api/db/core.py
@@ -9,7 +9,6 @@ os.makedirs(f"{PROJECT_ROOT}/data/user_data", exist_ok=True)
 
 DATABASE_URL = "sqlite:///./data/user_data/storage.db"
 
-engine = create_engine(DATABASE_URL)
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
